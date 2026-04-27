35f16e9aa5c2304d63037614ebb4b78b9ca95fbc
KEVINWONG <easymoneysniperchris@gmail.com>
2026-04-28 00:25:12 +0800

    Update login_screen.dart

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import '../student/student_dashboard.dart';
 
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Login"),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (_) => const StudentDashboard(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
