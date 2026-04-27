552b955d79fce77043758ff48440f4002e2f518e
KEVINWONG <easymoneysniperchris@gmail.com>
2026-04-28 00:24:50 +0800

    Update booking_service.dart

--- lib/services/booking_service.dart
+++ lib/services/booking_service.dart
@@ -1,1 +1,15 @@
+import '../models/booking.dart';
 
+class BookingService {
+  static final List<Booking> _bookings = [];
+
+  static List<Booking> get bookings => _bookings;
+
+  static void addBooking(Booking booking) {
+    _bookings.add(booking);
+  }
+
+  static void cancelBooking(Booking booking) {
+    booking.status = 'Cancelled';
+  }
+}
