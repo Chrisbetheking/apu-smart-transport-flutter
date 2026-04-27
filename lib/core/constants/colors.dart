f7be400085c6463acd74ed7b46ffa0e6337f9cc0
KEVINWONG <easymoneysniperchris@gmail.com>
2026-04-28 00:24:14 +0800

    Update colors.dart

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,1 +1,9 @@
+import 'package:flutter/material.dart';
 
+class AppColors {
+  static const primary = Color(0xFF3949AB);
+  static const secondary = Color(0xFF5C6BC0);
+  static const background = Color(0xFFF5F6FA);
+  static const success = Color(0xFF2E7D32);
+  static const danger = Color(0xFFC62828);
+}
