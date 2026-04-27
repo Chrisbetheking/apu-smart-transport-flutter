ae1ff3c1aa717757fc3de7836cd6f32e4ca16b0b
KEVINWONG <easymoneysniperchris@gmail.com>
2026-04-28 00:24:31 +0800

    Update booking.dart

--- lib/models/booking.dart
+++ lib/models/booking.dart
@@ -1,1 +1,11 @@
+class Booking {
+  final String id;
+  final String title;
+  String status;
 
+  Booking({
+    required this.id,
+    required this.title,
+    this.status = 'Reserved',
+  });
+}
