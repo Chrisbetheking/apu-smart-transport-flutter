7fad7a49ee511303bc20f26259b995155beb3d38
KEVINWONG <easymoneysniperchris@gmail.com>
2026-04-28 00:23:53 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/auth/login_screen.dart';
+import 'core/constants/colors.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'APU Smart Transport',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: AppColors.primary,
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
